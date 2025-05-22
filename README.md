19afdbbff121a7dc8caa348bd2e0a637dbaef52e
CodingComet <68121617+CodingComet@users.noreply.github.com>
2025-05-22 18:59:40 +0300

    Counts and visualizes number of occurences of each server.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Tonic Data Engineering Challenge
+
+### playground.py
+Generates random issues with predefined server list.
+
+### main.py
+Fetches and performs analysis.
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from jira import JIRA
 import time
-import dotenv
 import os
-
+import numpy as np
+import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
 # === CONFIGURATION ===
@@ -16,13 +16,61 @@ PROJECT_KEY = os.getenv("PROJECT_KEY")
 # === CONNECT TO JIRA ===
 jira = JIRA(server=JIRA_SERVER, basic_auth=(EMAIL, API_TOKEN))
 
-issue_dict = {
-    "project": {"key": PROJECT_KEY},
-    "summary": f"TEST SINGLE ISSUE",
-    "issuetype": {"name": "Task"},
-}
-try:
-    issue = jira.create_issue(fields=issue_dict)
-    print(f"Created: {issue.key}")
-except Exception as e:
-    print(f"Failed to create issue: {e}")
+
+def fetch_issues(start_at: int, max_results: int):
+    issues = jira.search_issues(
+        f"project={PROJECT_KEY}",  # JQL query to filter by project
+        fields="description",
+        startAt=start_at,
+        maxResults=max_results,
+    )
+    return issues
+
+
+def fetch_all_issues(max_results: int = 8):
+    issues = []
+    start_at = 0
+    while True:
+        last_issues = fetch_issues(start_at, max_results)
+        if not last_issues:
+            break
+        issues.extend(last_issues)
+        start_at += max_results
+    return issues
+
+
+def count_server_occurences(servers: list[str]):
+    all_issues = fetch_all_issues(50)
+    all_descriptions = [issue.fields.description for issue in all_issues]
+
+    no_associated_servers = 0
+    occurences = np.zeros(len(servers))
+
+    for description in all_descriptions:
+        lower = description.lower()
+        associated = np.array(
+            [lower.count(server_name) for server_name in server_names]
+        )
+        if np.all(associated == 0):
+            no_associated_servers += 1
+        else:
+            occurences += associated
+    return no_associated_servers, occurences
+
+
+# fetch_all_issues()
+server_names = ["srv-a", "srv-1", "srv-database3", "srv-00623"]
+no_server_mentioned, occurences = count_server_occurences(server_names)
+
+
+all_titles = np.append(server_names, "NO SERVER MENTIONED")
+all_amounts = np.append(occurences, no_server_mentioned)
+
+# Bar colors: blue for existing, red for extra
+colors = ["blue"] * len(occurences) + ["red"]
+
+# Plotting
+plt.bar(all_titles, all_amounts, color=colors)
+plt.ylabel("#")
+plt.title("Mentions of Each Server")
+plt.show()
